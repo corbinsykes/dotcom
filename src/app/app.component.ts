6e9a4c218debb6a688967b37ebeaf5e11a0269f9
Corbin Elliott Sykes <corbin@kargo.com>
2017-08-31 16:53:08 -0400

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,47 @@
 import { Component, HostListener, Inject, ViewEncapsulation, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { DOCUMENT} from '@angular/common';
 
 @Component({
     selector: 'app-root',
     encapsulation: ViewEncapsulation.None,
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
+    animations: [
+        trigger('itemAnimation', [
+            state('void', style({
+                transform: 'translateX(-100%)',
+                opacity: 0
+            })),
+            transition('void => first', [
+                animate('436ms ease', style({
+                    transform: 'translateX(0)',
+                    opacity: 1
+                }))
+            ]),
+            transition('void => second', [
+                animate('436ms 218ms ease', style({
+                    transform: 'translateX(0)',
+                    opacity: 1
+                }))
+            ]),
+            transition('void => third', [
+                animate('436ms 436ms ease', style({
+                    transform: 'translateX(0)',
+                    opacity: 1
+                }))
+            ]),
+            transition('void => fourth', [
+                animate('436ms 654ms ease', style({
+                    transform: 'translateX(0)',
+                    opacity: 1
+                }))
+            ]),
+            transition('* => void', [
+                animate('436ms ease', style({transform: 'translateX(100%)'}))
+            ])
+        ])
+    ]
 })
 export class AppComponent {
     @ViewChild('landing') landing;
